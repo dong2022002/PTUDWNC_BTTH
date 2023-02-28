918286a96970c85f34a20c3d94d94df3f1e86ffc
TranDieuDong <dong200220@gmail.com>
2023-02-28 22:26:16 +0700

    6. Create BlogRepository

---
+++ src/TipsAndTricks/TatBlog.Services/Blogs/BlogRepository.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TatBlog.Core.Entities;
+using TatBlog.Data.Contexts;
+
+namespace TatBlog.Services.Blogs
+{
+    public class BlogRepository : IBlogRepository
+    {
+        private readonly BlogDbContext _context;
+
+        public BlogRepository(BlogDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IList<Post>> GetPopularArticlesAsync(int numPosts, CancellationToken cancellationToken = default)
+        {
+           return await _context.Set<Post>()
+                .Include(x => x.Author)
+                .Include(x => x.Category)
+                .OrderByDescending(p => p.ViewCount)
+                .Take(numPosts)
+                .ToListAsync(cancellationToken);
+
+        }
+
+        public async Task<Post> GetPostAsync(int year, int month, string slug, CancellationToken cancellationToken = default)
+        {
+            IQueryable<Post> postQuery = _context.Set<Post>()
+               .Include(x => x.Category)
+               .Include(x => x.Author);
+
+            if (year > 0)
+            {
+                postQuery = postQuery.Where(x => x.PostedDate.Year == year);
+            }
+
+            if (month >0)
+            {
+                postQuery = postQuery.Where(x => x.PostedDate.Month == month);
+            }
+
+            if (!string.IsNullOrWhiteSpace(slug))
+            {
+                postQuery = postQuery.Where(x => x.UrlSlug == slug);
+            }
+
+            return await postQuery.FirstOrDefaultAsync(cancellationToken);
+        }
+
+        public async Task IncreaseViewCountAsync(
+            int postId, 
+            CancellationToken cancellationToken = default)
+        {
+           await _context.Set<Post>()
+                .Where(x => x.Id == postId)
+                .ExecuteUpdateAsync(p =>
+                    p.SetProperty(x => x.ViewCount, x => x.ViewCount +1),
+                    cancellationToken);
+        }
+
+        public async Task<bool> IsPostSlugExitedAsync(
+            int postId, 
+            string slug,
+            CancellationToken cancellationToken = default)
+        {
+            return await _context.Set<Post>()
+                .AnyAsync(x => x.Id != postId && x.UrlSlug == slug, 
+                cancellationToken);
+        }
+    }
+}
