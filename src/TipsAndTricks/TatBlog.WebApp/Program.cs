73272b39992d74f27728d33bc11edb0effb5672a
TranDieuDong <dong200220@gmail.com>
2023-03-07 20:11:36 +0700

    update program and run app web

--- src/TipsAndTricks/TatBlog.WebApp/Program.cs
+++ src/TipsAndTricks/TatBlog.WebApp/Program.cs
@@ -1,6 +1,28 @@
 var builder = WebApplication.CreateBuilder(args);
+{
+	builder.Services.AddControllersWithViews();
+}
 var app = builder.Build();
+{
+	if (app.Environment.IsDevelopment())
+	{
+		app.UseDeveloperExceptionPage();
+	}
+	else
+	{
+		app.UseExceptionHandler("/Blog/Error");
+		app.UseHsts();
+	}
 
-app.MapGet("/", () => "Hello World!");
+	app.UseHttpsRedirection();
 
+	app.UseStaticFiles();
+
+	app.UseRouting();
+
+	app.MapControllerRoute(
+		name: "default",
+		pattern: "{controller=Blog}/{action=Index}/{id?}");
+}
 app.Run();
+
