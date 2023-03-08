4f6cfc5b22b4348d9ddc55a2f78a1ab13be564fa
TranDieuDong <dong200220@gmail.com>
2023-03-08 09:49:46 +0700

    configure connectionString and update DbContext, Program

--- src/TipsAndTricks/TatBlog.Data/Contexts/BlogDbContext.cs
+++ src/TipsAndTricks/TatBlog.Data/Contexts/BlogDbContext.cs
@@ -15,14 +15,18 @@ namespace TatBlog.Data.Contexts
         public DbSet<Category> Categories { get; set; }
         public DbSet<Post> Posts { get; set; }
         public DbSet<Tag> Tags { get; set; }
-
         public DbSet<Subscriber> Subscribers { get; set; }
 
-        protected override void OnConfiguring(
-            DbContextOptionsBuilder optionsBuilder)
+        public BlogDbContext(DbContextOptions<BlogDbContext> options):base(options)
         {
-            optionsBuilder.UseSqlServer(@"Server=MSI;Database=TatBlog;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=True");
+            
         }
+
+        //protected override void OnConfiguring(
+        //    DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer(@"Server=MSI;Database=TatBlog;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=True");
+        //}
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfigurationsFromAssembly(
--- src/TipsAndTricks/TatBlog.WebApp/Program.cs
+++ src/TipsAndTricks/TatBlog.WebApp/Program.cs
@@ -1,7 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using TatBlog.Data.Contexts;
+using TatBlog.Data.Seeders;
+using TatBlog.Services.Blogs;
+
 var builder = WebApplication.CreateBuilder(args);
 {
 	builder.Services.AddControllersWithViews();
+
+	builder.Services.AddDbContext<BlogDbContext>(options =>
+		options.UseSqlServer(
+				builder.Configuration.GetConnectionString("DefaultConnection")));
+
+	builder.Services.AddScoped<IBlogRepository,BlogRepository>();
+	builder.Services.AddScoped<IDataSeeder,IDataSeeder>();
 }
+
 var app = builder.Build();
 {
 	if (app.Environment.IsDevelopment())
@@ -10,6 +23,7 @@ var app = builder.Build();
 	}
 	else
 	{
+
 		app.UseExceptionHandler("/Blog/Error");
 		app.UseHsts();
 	}
@@ -23,6 +37,13 @@ var app = builder.Build();
 	app.MapControllerRoute(
 		name: "default",
 		pattern: "{controller=Blog}/{action=Index}/{id?}");
+
+	
+}
+using (var scope = app.Services.CreateScope())
+{
+	var seeder = scope.ServiceProvider.GetRequiredService<IDataSeeder>();
+	seeder.Initialize();
 }
 app.Run();
 
