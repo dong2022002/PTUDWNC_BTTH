9ac2641738c00767ee733d2bd390a80392520b9b
TranDieuDong <dong200220@gmail.com>
2023-03-10 16:20:39 +0700

    create action Subscribe and Unsubscribe

---
+++ src/TipsAndTricks/TatBlog.WebApp/Controllers/NewsletterController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TatBlog.Services.Blogs;
+
+namespace TatBlog.WebApp.Controllers
+{
+	public class NewsletterController : Controller
+	{
+		private readonly IBlogRepository _blogRepository;
+
+		public NewsletterController(IBlogRepository blogRepository)
+		{
+			_blogRepository = blogRepository;
+		}
+		public async Task<IActionResult> Subscribe(
+			string email)
+		{
+			return View();
+		}public async Task<IActionResult> Unsubscribe(
+			string email)
+		{
+			return View();
+		}
+	}
+}
