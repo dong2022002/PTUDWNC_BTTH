61d3a92ea00c412f3ad688afaa7bff677ef096f7
TranDieuDong <dong200220@gmail.com>
2023-03-17 16:27:00 +0700

    create and update View,Controller,Action Categories

--- src/TipsAndTricks/TatBlog.WebApp/Areas/Admin/Controllers/CategoriesController.cs
+++ src/TipsAndTricks/TatBlog.WebApp/Areas/Admin/Controllers/CategoriesController.cs
@@ -1,12 +1,54 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FluentValidation;
+using MapsterMapper;
+using Microsoft.AspNetCore.Mvc;
+using TatBlog.Core.DTO;
+using TatBlog.Services.Blogs;
+using TatBlog.Services.Media;
+using TatBlog.WebApp.Areas.Admin.Models;
 
 namespace TatBlog.WebApp.Areas.Admin.Controllers
 {
 	public class CategoriesController : Controller
 	{
-		public IActionResult Index()
+		private readonly IBlogRepository _blogRepository;
+		private readonly IMapper _mapper;
+		private readonly IMediaManager _mediaManager;
+		private readonly IValidator<PostEditModel> _validator;
+		private readonly ILogger<PostsController> _logger;
+
+
+		public CategoriesController(
+			ILogger<PostsController> logger,
+			IValidator<PostEditModel> validator,
+			IMediaManager mediaManager,
+			IBlogRepository blogRepository,
+			IMapper mapper)
 		{
-			return View();
+			_logger = logger;
+			_validator = validator;
+			_blogRepository = blogRepository;
+			_mapper = mapper;
+			_mediaManager = mediaManager;
+		}
+		public async Task<IActionResult> Index(
+		CategoryFilterModel model,
+		[FromQuery(Name = "p")] int pageNumber = 1,
+		[FromQuery(Name = "ps")] int pageSize = 5)
+		{
+
+			_logger.LogInformation("Tạo điều kiện truy vấn");
+			var postQuery = _mapper.Map<PostQuery>(model);
+
+			_logger.LogInformation("Lấy danh sách bài viết từ CSDL");
+
+			ViewBag.PostsList = await _blogRepository
+				.GetPagedPostsAsync(postQuery, pageNumber, pageSize);
+
+			_logger.LogInformation("Chuẩn bị dữ liệu cho ViewModel");
+
+			//await PopulatePostFilterModelAsync(model);
+
+			return View(model);
 		}
 	}
 }
--- src/TipsAndTricks/TatBlog.WebApp/Areas/Admin/Models/CategoryFilterModel.cs
+++ src/TipsAndTricks/TatBlog.WebApp/Areas/Admin/Models/CategoryFilterModel.cs
@@ -1,6 +1,13 @@
-﻿namespace TatBlog.WebApp.Areas.Admin.Models
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace TatBlog.WebApp.Areas.Admin.Models
 {
 	public class CategoryFilterModel
 	{
+		public string? keyword { get; set; }
+		public string? Name { get; set; }
+		public string? UrlSlug { get; set; }
+		public string? Description { get; set; }
+		public bool isShowOnMenu { get; set; } = false;
 	}
 }
