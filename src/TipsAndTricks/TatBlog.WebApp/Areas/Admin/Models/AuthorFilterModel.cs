2a26c1f0ea9efcc857b083974f6affe8115ff4e8
TranDieuDong <dong200220@gmail.com>
2023-03-17 22:33:38 +0700

    create Author Filter,Edit,Query and View Filter,Index,PageList

---
+++ src/TipsAndTricks/TatBlog.WebApp/Areas/Admin/Models/AuthorFilterModel.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Globalization;
+
+namespace TatBlog.WebApp.Areas.Admin.Models
+{
+	public class AuthorFilterModel
+	{
+		public string? keyword { get; set; }
+		public string? FullName { get; set; }
+		public string? Email { get; set; }
+		public int? Year { get; set; }
+		public int? Month { get; set; }
+		public IEnumerable<SelectListItem>? MonthList { get; set; }
+
+		public AuthorFilterModel()
+		{
+			MonthList = Enumerable.Range(1, 12)
+				.Select(m => new SelectListItem()
+				{
+					Value = m.ToString(),
+					Text = CultureInfo.CurrentCulture
+					.DateTimeFormat.GetMonthName(m)
+				})
+				.ToList();
+		}
+	}
+}
