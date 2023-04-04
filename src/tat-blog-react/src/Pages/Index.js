681635468a0d31d0d0e9a49d00bb4700aa2a4aa6
TranDieuDong <dong200220@gmail.com>
2023-04-04 21:49:25 +0700

    update PageList Post

--- src/tat-blog-react/src/Pages/Index.js
+++ src/tat-blog-react/src/Pages/Index.js
@@ -1,27 +1,48 @@
 import React, { useEffect,useState } from 'react'
 import PostItem from '../components/PostItem'
 import { getPosts } from '../Services/BlogRepository';
+import { useLocation } from 'react-router-dom';
+import Pager from '../components/Pager';
 
 const Index = () => {
   const [postList,setPostList] = useState([]);
+  const [metadata,setMetadata] = useState([]);
+
+  function useQuery() {
+    const {search} = useLocation();
+    return React.useMemo(()=> new URLSearchParams(search),[search]);
+  }
+
+  let query = useQuery(),
+    k = query.get('k') ?? '',
+    p = query.get('p') ?? 1,
+    ps = query.get('ps')?? 10;
+
   useEffect(() => {
     document.title = 'Trang chủ';
 
-    getPosts().then(data =>{
+    getPosts(k,ps,p).then(data =>{
       if (data) {
         setPostList(data.items);
+        setMetadata(data.metadata)
       }else
       setPostList([]);
     })
-  }, []);
+  }, [k,p,ps]);
+
+  useEffect(()=>{
+    window.scrollTo(0,0);
+  },[postList]);
+
   if (postList.length>0) {
-    return (
+      return (
       <div className="p-4">
         {postList.map((item,index) =>{
           return(
             <PostItem postItem={item} key={index}/>
           )
         })}
+        <Pager postQuery={{'keyword':k}} metadata={metadata}/>
       </div>
     )
   }else return(
---
+++ src/tat-blog-react/src/components/Pager.js
@@ -0,0 +1,51 @@
+import { Link } from "react-router-dom"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import Button from "react-bootstrap/Button"
+import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
+
+const Pager = ({postQuery, metadata}) => {
+    let pageCount = metadata.pageCount,
+    hasNextPage = metadata.hasNextPage,
+    hasPreviousPage = metadata.hasPreviousPage,
+    pageNumber = metadata.pageNumber,
+    pageSize = metadata.pageSize,
+    actionName ='', slug = '',
+    keyword = postQuery.keyword ?? '';
+  if (pageCount>1) {
+    return (
+      <div className="text-center my-4">
+        {hasPreviousPage ?
+        <Link
+        to={`/blog/${actionName}?slug=${slug}&k=${keyword}&p=${pageNumber - 1}&ps=${pageSize}`}
+        className="btn btn-info">
+            <FontAwesomeIcon icon={faArrowLeft}/>
+            &nbsp;Trang trước
+        </Link>
+        :<Button variant="outline-secondary" disabled>
+            <FontAwesomeIcon icon={faArrowLeft}/>
+            &nbsp;Trang trước
+        </Button>
+        }
+          {hasNextPage ?
+        <Link
+        to={`/blog/${actionName}?slug=${slug}&k=${keyword}&p=${pageNumber + 1}&ps=${pageSize}`}
+        className="btn btn-info ms-1">
+            <FontAwesomeIcon icon={faArrowRight}/>
+            Trang sau&nbsp;
+        </Link>
+        :<Button variant="outline-secondary"
+        className="ms-1" disabled>
+            <FontAwesomeIcon icon={faArrowRight}/>
+            Trang sau&nbsp;
+        </Button>
+        }
+      </div>
+    )
+  }
+  return(
+    <Link>
+    </Link>
+  )
+}
+
+export default Pager
